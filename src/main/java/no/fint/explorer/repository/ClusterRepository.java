5f83da7dc863476418f0e313893c5380232f3ac0
torgravels <torleif@fintlabs.no>
2021-03-10 13:19:25 +0100

    Custom metric

--- src/main/java/no/fint/explorer/repository/ClusterRepository.java
+++ src/main/java/no/fint/explorer/repository/ClusterRepository.java
@@ -4,8 +4,10 @@ import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.ApiResponse;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.*;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
@@ -16,17 +18,19 @@ import java.util.Optional;
 @Repository
 public class ClusterRepository {
     private final CoreV1Api coreV1Api;
+    private final MeterRegistry meterRegistry;
 
     @Value("${kubernetes.namespace}")
     private String namespace;
 
     private final static String STACK = "fint.stack";
 
-    public ClusterRepository(CoreV1Api coreV1Api) {
+    public ClusterRepository(CoreV1Api coreV1Api, MeterRegistry meterRegistry) {
         this.coreV1Api = coreV1Api;
+        this.meterRegistry = meterRegistry;
     }
 
-    public Optional<ApiResponse<String>> getApiResponse(V1Service v1Service, String endpoint, String assetId) {
+    public Optional<ApiResponse<String>> getApiResponse(V1Service v1Service, String endpoint, String asset) {
         Optional<V1ObjectMeta> metadata = Optional.ofNullable(v1Service.getMetadata());
 
         String service = metadata
@@ -54,13 +58,19 @@ public class ClusterRepository {
 
         String path = label.replaceAll("-", "/") + endpoint;
 
-        coreV1Api.getApiClient().addDefaultHeader("x-org-id", assetId);
+        coreV1Api.getApiClient().addDefaultHeader("x-org-id", asset);
 
         try {
-            return Optional.ofNullable(coreV1Api.connectGetNamespacedServiceProxyWithPathWithHttpInfo(name, namespace, path, null));
+            Optional<ApiResponse<String>> apiResponse = Optional.ofNullable(coreV1Api.connectGetNamespacedServiceProxyWithPathWithHttpInfo(name, namespace, path, null));
+
+            apiResponse.ifPresent(response -> updateCounter(asset, service, endpoint, null));
+
+            return apiResponse;
 
         } catch (ApiException ex) {
-            log.error("{} - {} - {} - {}", assetId, service, endpoint, ex.getMessage());
+            log.error("{} - {} - {} - {}", asset, service, endpoint, ex.getMessage());
+
+            updateCounter(asset, service, endpoint, ex);
 
             return Optional.empty();
         }
@@ -85,6 +95,20 @@ public class ClusterRepository {
             log.error("{} - {}", label, ex.getResponseBody());
 
             return Collections.emptyList();
+        }
+    }
+
+    private void updateCounter(String asset, String service, String endpoint, ApiException apiException) {
+        Optional<ApiException> exception = Optional.ofNullable(apiException);
+
+        if (service.startsWith("consumer")) {
+            meterRegistry.counter("fint.consumer",
+                    "service", service,
+                    "asset", asset,
+                    "endpoint", endpoint,
+                    "status", exception.map(ApiException::getCode).map(String::valueOf).orElse(HttpStatus.OK.name()),
+                    "exception", exception.map(ApiException::getCause).map(Throwable::getClass).map(Class::getSimpleName).orElse("NONE"))
+                    .increment();
         }
     }
 }
