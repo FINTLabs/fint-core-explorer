d1d1d04cdadc853e488bcea32597c80a1ff6998b
torgravels <torleif@fintlabs.no>
2021-03-17 09:40:51 +0100

    Parameter

--- src/main/java/no/fint/explorer/controller/AssetController.java
+++ src/main/java/no/fint/explorer/controller/AssetController.java
@@ -4,14 +4,12 @@ import no.fint.explorer.exception.AssetNotFoundException;
 import no.fint.explorer.exception.ComponentNotFoundException;
 import no.fint.explorer.model.Asset;
 import no.fint.explorer.service.AssetService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("assets")
@@ -27,23 +25,17 @@ public class AssetController {
         return assetService.getAssets();
     }
 
-    @GetMapping("{id}")
-    public Asset getAsset(@PathVariable String id) {
-        return Optional.ofNullable(assetService.getAsset(id))
+    @GetMapping("{assetId}")
+    public Asset getAsset(@PathVariable String assetId) {
+        return Optional.ofNullable(assetService.getAsset(assetId))
                 .orElseThrow(AssetNotFoundException::new);
     }
 
-    @GetMapping("{id}/components")
-    public List<Asset.Component> getComponents(@PathVariable String id) {
-        return getAsset(id).getComponents();
-    }
-
-    @GetMapping("{assetId}/components/{componentId}")
-    public Asset.Component getComponent(@PathVariable String assetId, @PathVariable String componentId) {
-        return getComponents(assetId)
+    @GetMapping("{assetId}/components")
+    public List<Asset.Component> getComponents(@PathVariable String assetId, @RequestParam(required = false) String id) {
+        return getAsset(assetId).getComponents()
                 .stream()
-                .filter(component -> component.getId().equals(componentId))
-                .findFirst()
-                .orElseThrow(ComponentNotFoundException::new);
+                .filter(component -> Optional.ofNullable(id).map(component.getId()::contains).orElse(true))
+                .collect(Collectors.toList());
     }
 }
