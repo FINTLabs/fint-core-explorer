0832c5da5e3f01032acad13ac9649e2f1a557fcd
Hknots <hknots99@gmail.com>
2026-06-24 12:22:49 +0200

    feature: add FintProperties to list up the components we are going to perform health checks on

--- src/main/java/no/novari/fint/explorer/Application.java
+++ src/main/java/no/novari/fint/explorer/Application.java
@@ -2,12 +2,14 @@ package no.novari.fint.explorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/no/novari/fint/explorer/model/Component.kt
@@ -0,0 +1,14 @@
+package no.novari.fint.explorer.model
+
+@JvmInline
+value class Component(
+    private val value: String
+) {
+    init {
+        require(value.split("-").size == 2) { "Component is expected to have exactly 2 parts (domain-package), got: $value" }
+        require(value == value.lowercase()) { "Component is expected to be lowercase, got: $value" }
+    }
+
+    val domainName: String get() = value.split("-").first()
+    val packageName: String get() = value.split("-").last()
+}
