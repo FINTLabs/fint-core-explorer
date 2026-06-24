458d049601ba6989e71131699073e1dcc29d1274
Hknots <hknots99@gmail.com>
2026-06-24 14:15:46 +0200

    refactor: add connection and read timeout to RestClientConfig

--- src/main/java/no/novari/fint/explorer/config/RestClientConfig.kt
+++ src/main/java/no/novari/fint/explorer/config/RestClientConfig.kt
@@ -2,15 +2,28 @@ package no.novari.fint.explorer.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.JdkClientHttpRequestFactory
 import org.springframework.web.client.RestClient
+import java.net.http.HttpClient
+import java.time.Duration
 
 @Configuration
 class RestClientConfig {
 
     @Bean
-    fun restClient() =
-        RestClient.builder()
+    fun restClient(): RestClient {
+        val httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(15))
+            .build()
+
+        val requestFactory = JdkClientHttpRequestFactory(httpClient).apply {
+            setReadTimeout(Duration.ofSeconds(15))
+        }
+
+        return RestClient.builder()
+            .requestFactory(requestFactory)
             .defaultHeader("x-client", "fint-core-explorer")
             .build()
+    }
 
 }
